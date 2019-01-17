965d3fad5f27767f3f378c0503b4ec6239d7b165
Linda Chang <lindachang186@gmail.com>
2019-01-17 17:29:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,12 +25,12 @@ class Student
     end
   end
 
-  def self.drop_table
-    sql= <<-SQL
-    DROP TABLE students
-    SQL
-    DB[:conn].execute(sql)
-  end
+  # def self.drop_table
+  #   sql= <<-SQL
+  #   DROP TABLE students
+  #   SQL
+  #   DB[:conn].execute(sql)
+  # end
 
 #   def save
 #     if self.id

1458faaddfc7a09bdea2f5f8fc81f90bc08d2491
Linda Chang <lindachang186@gmail.com>
2019-01-17 17:32:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,8 +44,8 @@ class Student
     end
   end
 
-  def self.create
-    student = self.new
+  def self.create(name, grade)
+    student = Student.new(name, grade)
     student.save
     student
   end

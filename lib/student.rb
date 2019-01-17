d9f762d15fbc4b1f00bf71b29cfed446d03429cc
Linda Chang <lindachang186@gmail.com>
2019-01-17 17:42:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,9 +66,9 @@ class Student
       LIMIT 1
       SQL
 
-        DB[:conn].execute(sql, name).map do |row|
-        self.new_from_db(row)
-        end.first
+    DB[:conn].execute(sql, name).map do |row|
+    self.new_from_db(row)
+      end
     end
 
 

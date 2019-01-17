1ea4097b7a9804d04fd7c522cf18637291cb47ce
Linda Chang <lindachang186@gmail.com>
2019-01-17 17:45:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,6 +53,7 @@ class Student
   def self.new_from_db(row)
    new_student = Student.new(
    new_student.id = row[0],
+   binding.pry
    new_student.name = row[1],
    new_student.grade = row[2])
    new_student

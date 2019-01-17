92bd404ebc6bdaf80684f295badddbfd1e4446d5
Linda Chang <lindachang186@gmail.com>
2019-01-17 18:04:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
   end
 
   def self.new_from_db(row)
-   new_student = Student.new
+   new_student = Student.new(new_student.name, new_student.grade, new_student.id)
    new_student.name = row[1]
    new_student.grade = row[2]
    new_student.id = row[0]

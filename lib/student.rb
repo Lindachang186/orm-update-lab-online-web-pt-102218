6224b659837bcac01e4b37a0089ca120b9578415
Linda Chang <lindachang186@gmail.com>
2019-01-17 17:29:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,49 +32,49 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def save
-    if self.id
-    self.update
-  else
-    sql = <<-SQL
-      INSERT INTO songs (name, album)
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.album)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
-    end
-  end
-
-  def self.create(name:, grade:)
-    student = self.new(name, grade)
-    student.save
-    student
-  end
-
-  def self.new_from_db
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
-  end
-
-  def self.find_by_name(name)
-    sql= <<-SQL
-    SELECT *
-    FROM students
-    WHERE name = ?
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-
-
-def update
-  sql= "UPDATE students SET name ?, SET grade ?, WHERE ID = ?"
-  DB[:conn].execute(sql, self.name, self.grade, self.id)
-end
+#   def save
+#     if self.id
+#     self.update
+#   else
+#     sql = <<-SQL
+#       INSERT INTO songs (name, album)
+#       VALUES (?, ?)
+#     SQL
+#     DB[:conn].execute(sql, self.name, self.album)
+#     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+#     end
+#   end
+#
+#   def self.create(name:, grade:)
+#     student = self.new(name, grade)
+#     student.save
+#     student
+#   end
+#
+#   def self.new_from_db
+#     new_student = self.new
+#     new_student.id = row[0]
+#     new_student.name = row[1]
+#     new_student.grade = row[2]
+#     new_student
+#   end
+#
+#   def self.find_by_name(name)
+#     sql= <<-SQL
+#     SELECT *
+#     FROM students
+#     WHERE name = ?
+#     LIMIT 1
+#     SQL
+#     DB[:conn].execute(sql, name).map do |row|
+#       self.new_from_db(row)
+#     end.first
+#   end
+#
+#
+# def update
+#   sql= "UPDATE students SET name ?, SET grade ?, WHERE ID = ?"
+#   DB[:conn].execute(sql, self.name, self.grade, self.id)
+# end
 
 end
